6acf4458b48a97886c531d6e08188dad65edf10b
amcolab <amcolabdev@gmail.com>
2023-04-26 02:17:56 +0700

    config, connect, query database

--- apps/controllers/admin.js
+++ apps/controllers/admin.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+const {User} = require('../../db/models');
+
+router.get("/", async function(req, res) {
+  const users = await User.findAll();
+  console.log('all User: ', JSON.stringify(users, null, 2));
   res.json({"message": "this is admin page"});
 });
 
