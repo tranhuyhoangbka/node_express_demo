f7ee4654b5d05a688423c4a6669d0cf3cff921ce
tranhuyhoangbka <hoangth92.nd@gmail.com>
2023-05-06 17:19:15 +0700

    Graphql: Advance Pro Using

--- README.md
+++ README.md
@@ -11,5 +11,15 @@
 
 # graphql:
 
-https://topdev.vn/blog/huong-dan-bat-dau-graphql-voi-node-js/
+https://topdev.vn/blog/huong-dan-bat-dau-graphql-voi-node-js/ 
+<br>
 https://topdev.vn/blog/huong-dan-bat-dau-graphql-voi-node-js-phan-2/
+<br>
+https://www.digitalocean.com/community/tutorials/how-to-set-up-a-graphql-api-server-in-node-js
+<br>
+https://www.digitalocean.com/community/conceptual-articles/understanding-the-graphql-type-system
+<br>
+https://www.howtographql.com/graphql-js/1-getting-started/
+<br>
+### Using with sequelize:
+https://kinsta.com/blog/graphql-nodejs/
--- app.js
+++ app.js
@@ -1,50 +1,64 @@
 var express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 var { buildSchema } = require('graphql');
+var { makeExecutableSchema } = require('@graphql-tools/schema');
 var config = require('config');
 var bodyParser = require('body-parser');
 
 // Initialize a GraphQL schema
-var schema = buildSchema(`
-  type Query {
-    hello: String
-    user(id: Int!): Person
-    users(name: String): [Person]
-  },
-  type Mutation {
-    updateUser(id: Int!, firstName: String!, email: String): Person
-  },
-  type Person {
-    id: Int
-    firstName: String
-    lastName: String
-    email: String
-  }
-`);
+// var schema = buildSchema(`
+  const typeDefs = `
+    type Query {
+      hello: String
+      user(id: Int!): Person
+      users(name: String): [Person]
+    },
+    type Mutation {
+      updateUser(id: Int!, firstName: String!, email: String): Person
+    },
+    type Person {
+      id: Int!
+      firstName: String
+      lastName: String
+      email: String
+    }
+  `;
 
-const sampleUsers = [
-  {id: 1, firstName: 'a', lastName: 'b', email: 'hoangbk@gmail.com'},
-  {id: 2, firstName: 'a2', lastName: 'b2', email: 'hoangbk2@gmail.com'},
-  {id: 3, firstName: 'a3', lastName: 'b3', email: 'hoangbk3@gmail.com'},
-  {id: 4, firstName: 'a4', lastName: 'b4', email: 'hoangbk4@gmail.com'},
-]
+const sampleData = {
+  sampleUsers: [
+    {id: 1, firstName: 'a', lastName: 'b', email: 'hoangbk@gmail.com'},
+    {id: 2, firstName: 'a2', lastName: 'b2', email: 'hoangbk2@gmail.com'},
+    {id: 3, firstName: 'a3', lastName: 'b3', email: 'hoangbk3@gmail.com'},
+    {id: 4, firstName: 'a4', lastName: 'b4', email: 'hoangbk4@gmail.com'},
+  ]
+}
 
 // Root resolver
-var root = { 
-  hello: () => 'Hello world!',
-  user: (id) => (
-    {id: 1, firstName: 'a', lastName: 'b', email: 'hoangbk@gmail.com'}
-  ),
-  users: (args) => (
-    sampleUsers.filter(user => user.firstName.includes(args.name) || user.lastName.includes(args.name))
-  ),
-  updateUser: ({id, firstName, email}) => {
-    const user = sampleUsers.find(user => user.id === id);
-    user.firstName = firstName
-    user.email = email
-    return sampleUsers.find(user => user.id === id)
+const resolvers = {
+  Query: {
+    hello: (obj, args, context, info) => 'Hello world!',
+    user: (obj, args, context, info) => (
+      {id: 1, firstName: 'a', lastName: 'b', email: 'hoangbk@gmail.com'}
+    ),
+    users: (obj, args, context, info) => (
+      context.sampleUsers.filter(user => user.firstName.includes(args.name) || user.lastName.includes(args.name))
+    ),
+  },
+
+  Mutation: {
+    updateUser: (obj, args, context, info) => {
+      const user = context.sampleUsers.find(user => user.id === args.id);
+      user.firstName = args.firstName
+      user.email = args.email
+      return context.sampleUsers.find(user => user.id === args.id)
+    }
   }
 };
+
+const executableSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
 
 var app = express();
 // body parser
@@ -61,8 +75,10 @@ var controllers = require(__dirname + "/apps/controllers");
 app.use(controllers);
 
 app.use('/graphql', graphqlHTTP({
-  schema: schema,  // Must be provided
-  rootValue: root,
+  // schema: schema,  // Must be provided
+  schema: executableSchema,  // Must be provided
+  // rootValue: root,
+  context: sampleData,
   graphiql: true,  // Enable GraphiQL when server endpoint is accessed in browser
 }));
 
